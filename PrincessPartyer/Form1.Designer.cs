d2c7c43f99c0d273d5195d631fa52f93b788d9f6
Matt <MDSall7@gmail.com>
2019-03-11 16:32:25 -0500

    add files

--- PrincessPartyer/Form1.Designer.cs
+++ PrincessPartyer/Form1.Designer.cs
@@ -28,12 +28,407 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPartyAddress = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.calPartyDate = new System.Windows.Forms.DateTimePicker();
+            this.cmbPackages = new System.Windows.Forms.ComboBox();
+            this.clbExtras = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbPrincess = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.calBookDate = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbPaymentMethod = new System.Windows.Forms.ComboBox();
+            this.cmbReference = new System.Windows.Forms.ComboBox();
+            this.txtHomeAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtParentName = new System.Windows.Forms.TextBox();
+            this.txtChildAge = new System.Windows.Forms.TextBox();
+            this.txtChildName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(488, 308);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label13);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(480, 282);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Add Event";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Add Party";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.txtPartyAddress);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.calPartyDate);
+            this.groupBox2.Controls.Add(this.cmbPackages);
+            this.groupBox2.Controls.Add(this.clbExtras);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cmbPrincess);
+            this.groupBox2.Location = new System.Drawing.Point(243, 18);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(226, 209);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Party";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(0, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Party Address:";
+            // 
+            // txtPartyAddress
+            // 
+            this.txtPartyAddress.Location = new System.Drawing.Point(85, 183);
+            this.txtPartyAddress.Name = "txtPartyAddress";
+            this.txtPartyAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtPartyAddress.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(5, 161);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Party Date:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Package:";
+            // 
+            // calPartyDate
+            // 
+            this.calPartyDate.CustomFormat = "";
+            this.calPartyDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.calPartyDate.Location = new System.Drawing.Point(86, 157);
+            this.calPartyDate.Name = "calPartyDate";
+            this.calPartyDate.Size = new System.Drawing.Size(100, 20);
+            this.calPartyDate.TabIndex = 23;
+            // 
+            // cmbPackages
+            // 
+            this.cmbPackages.FormattingEnabled = true;
+            this.cmbPackages.Location = new System.Drawing.Point(85, 13);
+            this.cmbPackages.Name = "cmbPackages";
+            this.cmbPackages.Size = new System.Drawing.Size(121, 21);
+            this.cmbPackages.TabIndex = 0;
+            // 
+            // clbExtras
+            // 
+            this.clbExtras.Enabled = false;
+            this.clbExtras.FormattingEnabled = true;
+            this.clbExtras.Location = new System.Drawing.Point(86, 44);
+            this.clbExtras.Name = "clbExtras";
+            this.clbExtras.Size = new System.Drawing.Size(120, 79);
+            this.clbExtras.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Extras:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Princess:";
+            // 
+            // cmbPrincess
+            // 
+            this.cmbPrincess.FormattingEnabled = true;
+            this.cmbPrincess.Location = new System.Drawing.Point(85, 130);
+            this.cmbPrincess.Name = "cmbPrincess";
+            this.cmbPrincess.Size = new System.Drawing.Size(121, 21);
+            this.cmbPrincess.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.calBookDate);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbPaymentMethod);
+            this.groupBox1.Controls.Add(this.cmbReference);
+            this.groupBox1.Controls.Add(this.txtHomeAddress);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtParentName);
+            this.groupBox1.Controls.Add(this.txtChildAge);
+            this.groupBox1.Controls.Add(this.txtChildName);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(16, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 209);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(10, 174);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Booking Date:";
+            // 
+            // calBookDate
+            // 
+            this.calBookDate.CustomFormat = "";
+            this.calBookDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.calBookDate.Location = new System.Drawing.Point(91, 170);
+            this.calBookDate.Name = "calBookDate";
+            this.calBookDate.Size = new System.Drawing.Size(100, 20);
+            this.calBookDate.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Parent Name:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Home Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Payment:";
+            // 
+            // cmbPaymentMethod
+            // 
+            this.cmbPaymentMethod.FormattingEnabled = true;
+            this.cmbPaymentMethod.Location = new System.Drawing.Point(91, 143);
+            this.cmbPaymentMethod.Name = "cmbPaymentMethod";
+            this.cmbPaymentMethod.Size = new System.Drawing.Size(121, 21);
+            this.cmbPaymentMethod.TabIndex = 7;
+            // 
+            // cmbReference
+            // 
+            this.cmbReference.FormattingEnabled = true;
+            this.cmbReference.Location = new System.Drawing.Point(91, 116);
+            this.cmbReference.Name = "cmbReference";
+            this.cmbReference.Size = new System.Drawing.Size(121, 21);
+            this.cmbReference.TabIndex = 9;
+            // 
+            // txtHomeAddress
+            // 
+            this.txtHomeAddress.Location = new System.Drawing.Point(91, 90);
+            this.txtHomeAddress.Name = "txtHomeAddress";
+            this.txtHomeAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtHomeAddress.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Reference:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Child Age:";
+            // 
+            // txtParentName
+            // 
+            this.txtParentName.Location = new System.Drawing.Point(91, 12);
+            this.txtParentName.Name = "txtParentName";
+            this.txtParentName.Size = new System.Drawing.Size(100, 20);
+            this.txtParentName.TabIndex = 11;
+            // 
+            // txtChildAge
+            // 
+            this.txtChildAge.Location = new System.Drawing.Point(91, 64);
+            this.txtChildAge.Name = "txtChildAge";
+            this.txtChildAge.Size = new System.Drawing.Size(100, 20);
+            this.txtChildAge.TabIndex = 15;
+            // 
+            // txtChildName
+            // 
+            this.txtChildName.Location = new System.Drawing.Point(91, 38);
+            this.txtChildName.Name = "txtChildName";
+            this.txtChildName.Size = new System.Drawing.Size(100, 20);
+            this.txtChildName.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Child Name:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(480, 282);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(336, 248);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "label13";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 326);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox cmbPackages;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbReference;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbPaymentMethod;
+        private System.Windows.Forms.CheckedListBox clbExtras;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbPrincess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtHomeAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtParentName;
+        private System.Windows.Forms.TextBox txtChildAge;
+        private System.Windows.Forms.TextBox txtChildName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker calBookDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtPartyAddress;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker calPartyDate;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- PrincessPartyer/Form1.cs
+++ PrincessPartyer/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Windows.Forms;
 
 namespace PrincessPartyer
@@ -15,6 +9,90 @@ namespace PrincessPartyer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SQLiteConnection m_dbConnection;
+
+            m_dbConnection = new SQLiteConnection("Data Source=C:/Users/Matt/Documents/sqllite/WUAS.db;Version=3;");
+            m_dbConnection.Open();
+
+            SQLiteCommand command = new SQLiteCommand("select Package from packages", m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                cmbPackages.Items.Add(reader["Package"]);
+
+            command = new SQLiteCommand("select Extras from extras", m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+                clbExtras.Items.Add(reader["Extras"]);
+
+            command = new SQLiteCommand("select Princess from princesses", m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+                cmbPrincess.Items.Add(reader["Princess"]);
+
+            command = new SQLiteCommand("select PaymentMethod from paymentmethods", m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+                cmbPaymentMethod.Items.Add(reader["PaymentMethod"]);
+
+            command = new SQLiteCommand("select DiscoveredFrom from discovered", m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+                cmbReference.Items.Add(reader["DiscoveredFrom"]);
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        { }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=C:/Users/Matt/Documents/sqllite/WUAS.db;Version=3;");
+            m_dbConnection.Open();
+
+            string sqlcols, sqlvals, sql;
+            SQLiteCommand command;
+            SQLiteDataReader reader;
+            string lastEventID;
+
+            sqlcols = "insert into events (BookDate, EventDate,Princess,Package,Extras,PartyAddress) ";
+            sqlvals = "values ('";
+            sqlvals += calBookDate.Text + "', '";
+            sqlvals += calPartyDate.Text + "', '";
+            sqlvals += (cmbPrincess.SelectedIndex+1) + "', '";
+            sqlvals += (cmbPackages.SelectedIndex+1) + "', '";
+            sqlvals += "0" + "', '";
+            sqlvals += txtPartyAddress.Text + "')";
+            sql = sqlcols + sqlvals;
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            
+            SQLiteCommand cmd = new SQLiteCommand("SELECT last_insert_rowid() from events", m_dbConnection);
+            reader = cmd.ExecuteReader();
+            lastEventID = reader["last_insert_rowid()"].ToString();
+
+            sqlcols = "insert into customer (ParentName, ChildName, ChildAge, Address, Reference, EventID) ";
+            sqlvals = "values ('";
+            sqlvals += txtParentName.Text + "', '";
+            sqlvals += txtChildName.Text + "', '";
+            sqlvals += txtChildAge.Text + "', '";
+            sqlvals += txtHomeAddress.Text + "', '";
+            sqlvals += cmbReference.Text + "', '";
+            sqlvals += lastEventID + "')";
+            sql = sqlcols + sqlvals;
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label13.Text = cmbPrincess.SelectedIndex.ToString();
         }
     }
 }
